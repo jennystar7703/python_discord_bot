1670ac1cf03ac2d2b446ad88244e3471bec687d0
lucy-zeta <lucy@zetacube.net>
2023-06-28 12:13:24 +0900

    config.json íí+ .gitignore 파일 추가

--- bot.py
+++ bot.py
@@ -1,7 +1,19 @@
 import discord
 import requests
-from config import BOT_TOKEN, API__KEY, HEADERS, intents
+import json
 
+# json 파일에서 config.json 정보 가줘오기
+def load_config():
+    with open('config.json') as file:
+        return json.load(file)
+    
+config = load_config()
+BOT_TOKEN = config['BOT_TOKEN']
+    
+# discord 라이브러리 세팅 chatgpt 에서 가줘왔음 
+intents = discord.Intents.default()  # Create an instance of Intents
+intents.typing = False  # Disable typing event (optional)
+intents.presences = False  # Disable presence event (optional)
 client = discord.Client(intents=intents)  # Pass the intents argument
 
 @client.event
